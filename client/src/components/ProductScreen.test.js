fd9e4ba4fbbb9014fff6d6ec9c03c97ee5837c3e
Tanmoy Roy <157131159+Tanmoy0151@users.noreply.github.com>
2024-09-08 12:57:59 +0530

    Delete client directory
    rewrite clint directory

--- client/src/components/ProductScreen.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ProductScreen from './ProductScreen';
-
-test('renders product details', () => {
-    render(<ProductScreen match={{ params: { id: '1' } }} />);
-    const linkElement = screen.getByText(/Product Details/i);
-    expect(linkElement).toBeInTheDocument();
-});
